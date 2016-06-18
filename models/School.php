05549dabef8cfcb7bdec2f79db19952f90398a65
smallwhale <398164155@qq.com>
2016-06-18 10:25:27 +0800

    学习 models/School.php 修改

--- models/School.php
+++ models/School.php
@@ -17,7 +17,7 @@ use Yii;
  * @property string $city_id
  * @property string $s_linkman
  * @property string $s_tel
- * @property integer $s_phone
+ * @property string $s_phone
  * @property string $s_address
  * @property string $s_logo
  * @property string $s_intro
@@ -43,10 +43,11 @@ class School extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['u_id', 'n_id', 's_status', 'scale_id', 's_phone', 's_hits'], 'integer'],
+            [['u_id', 'n_id', 's_status', 'scale_id', 's_hits'], 'integer'],
             [['s_intro'], 'string'],
             [['s_license', 's_rca', 'city_id', 's_address', 's_logo'], 'string', 'max' => 50],
             [['s_linkman', 's_tel'], 'string', 'max' => 20],
+            [['s_phone'], 'string', 'max' => 11],
             [['s_y', 's_x', 's_website'], 'string', 'max' => 15],
             [['s_tags'], 'string', 'max' => 200]
         ];
