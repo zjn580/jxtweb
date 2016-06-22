fc78e779484023a85bb21572908db16371bc5129
kunge <549942346@qq.com>
2016-06-22 09:34:21 +0800

    公司 model

--- models/Company.php
+++ models/Company.php
@@ -12,19 +12,20 @@ use Yii;
  * @property integer $n_id
  * @property string $c_license
  * @property integer $c_status
+ * @property integer $l_id
  * @property integer $scale_id
  * @property string $c_rca
- * @property string $city_id
+ * @property integer $city_id
  * @property string $c_linkman
  * @property string $c_tel
- * @property integer $c_phone
+ * @property string $c_phone
  * @property string $c_address
  * @property string $c_logo
  * @property string $c_intro
- * @property integer $c_hits
- * @property string $c_y
  * @property string $c_x
+ * @property string $c_y
  * @property string $c_website
+ * @property string $c_tags
  */
 class Company extends \yii\db\ActiveRecord
 {
@@ -42,11 +43,12 @@ class Company extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['u_id', 'n_id', 'c_status', 'scale_id', 'c_phone'], 'integer'],
-            [['c_intro'], 'string'],
-            [['c_license', 'c_rca', 'city_id', 'c_address', 'c_logo'], 'string', 'max' => 50],
-            [['c_linkman', 'c_tel'], 'string', 'max' => 20],
-            [['c_y', 'c_x', 'c_website'], 'string', 'max' => 15]
+            [['u_id', 'n_id', 'c_status', 'l_id', 'scale_id', 'city_id'], 'integer'],
+            [['c_license', 'c_x', 'c_y'], 'string', 'max' => 50],
+            [['c_rca'], 'string', 'max' => 30],
+            [['c_linkman', 'c_tel', 'c_phone', 'c_address'], 'string', 'max' => 20],
+            [['c_logo'], 'string', 'max' => 100],
+            [['c_website', 'c_tags', 'c_intro'], 'string', 'max' => 255]
         ];
     }
 
@@ -61,6 +63,7 @@ class Company extends \yii\db\ActiveRecord
             'n_id' => 'N ID',
             'c_license' => 'C License',
             'c_status' => 'C Status',
+            'l_id' => 'L ID',
             'scale_id' => 'Scale ID',
             'c_rca' => 'C Rca',
             'city_id' => 'City ID',
@@ -70,10 +73,10 @@ class Company extends \yii\db\ActiveRecord
             'c_address' => 'C Address',
             'c_logo' => 'C Logo',
             'c_intro' => 'C Intro',
-            //'c_hits' => 'C Hits',
-            'c_y' => 'C Y',
             'c_x' => 'C X',
+            'c_y' => 'C Y',
             'c_website' => 'C Website',
+            'c_tags' => 'C Tags',
         ];
     }
 }
