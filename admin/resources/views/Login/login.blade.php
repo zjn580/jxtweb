90c6697e5ce12ca3a7133d337859e62932904841
smallwhale <398164155@qq.com>
2016-06-29 08:31:24 +0800

    登录

--- admin/resources/views/Login/login.blade.php
+++ admin/resources/views/Login/login.blade.php
@@ -10,17 +10,18 @@
     <h1>后台管理</h1>
     <div class="adming_login_border">
         <div class="admin_input">
-            <form action="index.html" method="post">
+            <form action="dologin" method="post">
                 <ul class="admin_items">
                     <li>
                         <label for="user">用户名：</label>
-                        <input type="text" name="username" value="admin" id="user" size="40" class="admin_input_style" />
+                        <input type="text" name="username" value="" id="user" size="40" class="admin_input_style" />
                     </li>
                     <li>
                         <label for="pwd">密码：</label>
-                        <input type="password" name="pwd" value="admin" id="pwd" size="40" class="admin_input_style" />
+                        <input type="password" name="pwd" value="" id="pwd" size="40" class="admin_input_style" />
                     </li>
                     <li>
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <input type="submit" tabindex="3" value="提交" class="btn btn-primary" />
                     </li>
                 </ul>
