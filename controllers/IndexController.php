9dfd3818b80bbafbb426c4293ff0f110ad259590
kunge <549942346@qq.com>
2016-06-30 18:35:50 +0800

    index主页遍历

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -8,15 +8,60 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\Industry;
+header('content-type:text/html;charset=utf8');
 
 class IndexController extends Controller
 {
 	public $layout = 'common';
 	//主页
     public function actionIndex()
-    {
-    	return $this->render('index');
+    {   
+        $Industry = new Industry;
+        $newarr = $Industry->showIndustry();
+        $query = (new \yii\db\Query())
+        ->select('*')
+        ->from('jx_position')
+        ->leftJoin('jx_salary','jx_salary.sa_id = jx_position.salary_id')
+        ->leftJoin('jx_experience', 'jx_experience.ex_id = jx_position.experience_id')
+        ->leftJoin('jx_city', 'jx_city.city_id = jx_position.city_id')
+        ->leftJoin('jx_edu', 'jx_edu.e_id = jx_position.e_id')
+        ->leftJoin('jx_company', 'jx_company.c_id = jx_position.c_id')
+        ->where(['is_up'=>0])
+        ->limit('10')
+        ->all();
+
+        $query1 = (new \yii\db\Query())
+        ->select('*')
+        ->from('jx_position')
+        ->leftJoin('jx_salary','jx_salary.sa_id = jx_position.salary_id')
+        ->leftJoin('jx_experience', 'jx_experience.ex_id = jx_position.experience_id')
+        ->leftJoin('jx_city', 'jx_city.city_id = jx_position.city_id')
+        ->leftJoin('jx_edu', 'jx_edu.e_id = jx_position.e_id')
+        ->leftJoin('jx_company', 'jx_company.c_id = jx_position.c_id')
+        ->where(['is_up'=>0])
+        ->orderby('p_time desc')
+        ->limit('10')
+        ->all();
+    	return $this->render('index',['arr'=>$query,'arr1'=>$query1,'newarr'=>$newarr]);
     }
+
+    //更多职位
+    public function actionIndexlist(){
+        $query = (new \yii\db\Query())
+        ->select('*')
+        ->from('jx_position')
+        ->leftJoin('jx_salary','jx_salary.sa_id = jx_position.salary_id')
+        ->leftJoin('jx_experience', 'jx_experience.ex_id = jx_position.experience_id')
+        ->leftJoin('jx_city', 'jx_city.city_id = jx_position.city_id')
+        ->leftJoin('jx_edu', 'jx_edu.e_id = jx_position.e_id')
+        ->leftJoin('jx_company', 'jx_company.c_id = jx_position.c_id')
+        ->where(['is_up'=>0])
+        ->all();
+
+        return $this->render('indexlist',['arr'=>$query]);
+    }
+
     //账号设置
     public function actionAccount()
     {
